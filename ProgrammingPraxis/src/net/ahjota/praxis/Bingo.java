c94003237994f8c76661e270ed74beefc9451844
AJ Alon <alexander.joel.alon@gmail.com>
2013-02-14 22:40:51 -0500

    added solution stub for Praxis exercise: Bingo

---
+++ ProgrammingPraxis/src/net/ahjota/praxis/Bingo.java
@@ -0,0 +1,26 @@
+package net.ahjota.praxis;
+
+/**
+ * Bingo is a children’s game of chance, sometimes played by adults for fun or
+ * money. Each player has a card of numbers arranged in a five-by-five grid with
+ * five randomly-chosen numbers from 1 to 15 in the first column, from 16 to 30
+ * in the second column, 31 to 45 in the third column, 46 to 60 in the fourth
+ * column, and 61 to 75 in the fifth column; the central space is “free” and is
+ * considered to be occupied. Then a caller randomly calls numbers from 1 to 75
+ * without replacement, each player marking the corresponding number, if it is
+ * present on their card, as occupied. The first player to have five occupied
+ * numbers in a row horizontally, in a column vertically, or along either of the
+ * two major diagonals is the winner.
+ * 
+ * What is the average number of calls required before a single card achieves
+ * bingo? In a large game with five hundred cards in play, what is the average
+ * number of calls required before any card achieves bingo?
+ * 
+ * @see <a href="http://programmingpraxis.com/2009/02/19/bingo/">Programming
+ *      Praxis Exercise 3</a>
+ * 
+ * @author AJ
+ */
+public class Bingo {
+
+}
