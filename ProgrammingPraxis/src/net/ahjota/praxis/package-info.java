cd4d33b0658405ae352fcf43ae9afa482596a71e
AJ Alon <alexander.joel.alon@gmail.com>
2013-02-14 22:34:37 -0500

    new Eclipse project java 'ProgrammingPraxis'

---
+++ ProgrammingPraxis/src/net/ahjota/praxis/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains implementations of solutions to the <a href="http://programmingpraxis.com/">Programming Praxis exercises</a>.
+ * 
+ * @author AJ
+ *
+ */
+package net.ahjota.praxis;
