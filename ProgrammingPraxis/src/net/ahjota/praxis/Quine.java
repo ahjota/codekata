62f3fd69d004f76043d38d06f49ee2edb3d3b889
AJ Alon <alexander.joel.alon@gmail.com>
2013-02-17 10:25:41 -0500

    beginning of first pass of "A Self Reproducing Program" solution

--- ProgrammingPraxis/src/net/ahjota/praxis/Quine.java
+++ ProgrammingPraxis/src/net/ahjota/praxis/Quine.java
@@ -5,16 +5,49 @@ package net.ahjota.praxis;
  * its complete output.
  * 
  * @see <a
- *      href=http://programmingpraxis.com/2009/02/20/a-self-reproducing-program/
+ *      href="http://programmingpraxis.com/2009/02/20/a-self-reproducing-program
+ *      /
  *      ">http://programmingpraxis.com/2009/02/20/a-self-reproducing-program/</a
  *      >
  * 
  * @author AJ
  */
 public class Quine {
-	
 	public static void main(String[] args) {
+		int charQuote = 34;
+		String srcCode[] = {// line 18
+				"package net.ahjota.praxis; ",
+				"",
+				"/**",
+				" * A program that takes no input and generates a copy of its own source text as",
+				" * its complete output.",
+				" * ",
+				" * @see <a",
+				" *      href=\"http://programmingpraxis.com/2009/02/20/a-self-reproducing-program",
+				" *      /",
+				" *      \">http://programmingpraxis.com/2009/02/20/a-self-reproducing-program/</a",
+				" *      >",
+				" * ",
+				" * @author AJ",
+				" */",
+				"public class Quine {",
+				"	public static void main(String[] args) {",
+				"		int charQuote = 34;",
+				"		String srcCode[] = {// line 18",
+				"...",
+				"	}",
+				"}"
+		};// line 39
 		
+		int i;
+		for (i = 0; i < 18; ++i) {
+			System.out.println(srcCode[i]);
+		}
+		for (String loc : srcCode) {
+			System.out.println(loc);
+		}
+		for (i = 39; i < srcCode.length; ++i) {
+			System.out.println(srcCode[i]);
+		}
 	}
-	
 }
