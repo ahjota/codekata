c3ff6719993644de53f15bdc008cd7cf5e057a5c
AJ Alon <alexander.joel.alon@gmail.com>
2013-02-17 09:58:26 -0500

    stub for Praxis exercise: A Self Reproducing Program

---
+++ ProgrammingPraxis/src/net/ahjota/praxis/Quine.java
@@ -0,0 +1,20 @@
+package net.ahjota.praxis;
+
+/**
+ * A program that takes no input and generates a copy of its own source text as
+ * its complete output.
+ * 
+ * @see <a
+ *      href=http://programmingpraxis.com/2009/02/20/a-self-reproducing-program/
+ *      ">http://programmingpraxis.com/2009/02/20/a-self-reproducing-program/</a
+ *      >
+ * 
+ * @author AJ
+ */
+public class Quine {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
