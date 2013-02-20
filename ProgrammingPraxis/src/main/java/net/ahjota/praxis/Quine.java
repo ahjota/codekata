32c6cefe85089f50bde983966164baa874eca378
AJ Alon <alexander.joel.alon@gmail.com>
2013-02-20 12:18:50 -0500

    Quine: removed unused 'charTab' var

--- ProgrammingPraxis/src/main/java/net/ahjota/praxis/Quine.java
+++ ProgrammingPraxis/src/main/java/net/ahjota/praxis/Quine.java
@@ -14,11 +14,10 @@ package net.ahjota.praxis;
  */
 public class Quine {
 	public static void main(String[] args) {
-		char charTab = 9;
-		String charTabs = new String(new char[]{charTab, charTab, charTab, charTab});
+		String charTabs = new String(new char[]{9, 9, 9, 9});
 		char charQuote = 34;
 		char charComma = 44;
-		String srcCode[] = {// line 21
+		String srcCode[] = {// line 20
 				
 				"package net.ahjota.praxis; ",
 				"",
@@ -36,23 +35,22 @@ public class Quine {
 				" */",
 				"public class Quine {",
 				"	public static void main(String[] args) {",
-				"		char charTab = 9;",
-				"		String charTabs = new String(new char[]{charTab, charTab, charTab, charTab});",
+				"		String charTabs = new String(new char[]{9, 9, 9, 9});",
 				"		char charQuote = 34;",
 				"		char charComma = 44;",
-				"		String srcCode[] = {// line 21",
+				"		String srcCode[] = {// line 20",
 				"",
 				"",
 				"		};",
 				"",
 				"		int i;",
-				"		for (i = 0; i < 22; ++i) {",
+				"		for (i = 0; i < 21; ++i) {",
 				"			System.out.println(srcCode[i]);// before srcCode definition",
 				" 		}",
 				"		for (String loc : srcCode) {",
 				"			System.out.println(charTabs + charQuote + loc + charQuote + charComma);",
 				"		}",
-				"		for (i = 22; i < srcCode.length; ++i) {",
+				"		for (i = 21; i < srcCode.length; ++i) {",
 				"			System.out.println(srcCode[i]);// after srcCode definition",
 				"		}",
 				"	}",
@@ -61,13 +59,13 @@ public class Quine {
 		};
 		
 		int i;
-		for (i = 0; i < 22; ++i) {
+		for (i = 0; i < 21; ++i) {
 			System.out.println(srcCode[i]);// before srcCode definition
 		}
 		for (String loc : srcCode) {
 			System.out.println(charTabs + charQuote + loc + charQuote + charComma);
 		}
-		for (i = 22; i < srcCode.length; ++i) {
+		for (i = 21; i < srcCode.length; ++i) {
 			System.out.println(srcCode[i]);// after srcCode definition
 		}
 	}
