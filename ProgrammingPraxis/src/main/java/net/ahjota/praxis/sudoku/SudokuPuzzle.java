4fe2792a187a0dc2ca5b312b1cc7b631fe2578ac
AJ Alon <alexander.joel.alon@gmail.com>
2013-02-26 14:42:54 -0500

    Sudoku: minor code cleanup

--- ProgrammingPraxis/src/main/java/net/ahjota/praxis/sudoku/SudokuPuzzle.java
+++ ProgrammingPraxis/src/main/java/net/ahjota/praxis/sudoku/SudokuPuzzle.java
@@ -21,11 +21,11 @@ public class SudokuPuzzle {
 	 * A1 A2 A3| A4 A5 A6| A7 A8 A9 
 	 * B1 B2 B3| B4 B5 B6| B7 B8 B9
 	 * C1 C2 C3| C4 C5 C6| C7 C8 C9 
-	 * ---------+---------+---------
+	 * --------+---------+---------
 	 * D1 D2 D3| D4 D5 D6| D7 D8 D9 
 	 * E1 E2 E3| E4 E5 E6| E7 E8 E9 
 	 * F1 F2 F3| F4 F5 F6| F7 F8 F9 
-	 * ---------+---------+---------
+	 * --------+---------+---------
 	 * G1 G2 G3| G4 G5 G6| G7 G8 G9 
 	 * H1 H2 H3| H4 H5 H6| H7 H8 H9 
 	 * I1 I2 I3| I4 I5 I6| I7 I8 I9
@@ -55,12 +55,6 @@ public class SudokuPuzzle {
 	 *            characters are ignored.
 	 */
 	private void parseGridState(String initialGridState) {
-		/*
-		 * 1. filter the incoming grid state argument
-		 * 2. initialize the map, with all possible values
-		 * 3. set values based on incoming grid state
-		 */
-		
 		if (null == initialGridState) {
 			throw new NullPointerException("initialGridState is null");
 		}
@@ -103,6 +97,10 @@ public class SudokuPuzzle {
 		}
 	}
 	
+	private String label(char rowLetter, char colNumber) {
+		return "" + rowLetter + colNumber;
+	}
+	
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		for (Entry<String, String> entry : values.entrySet()) {
@@ -110,10 +108,6 @@ public class SudokuPuzzle {
 		}
 		
 		return builder.toString();
-	}
-	
-	private String label(char rowLetter, char colNumber) {
-		return "" + rowLetter + colNumber;
 	}
 	
 	public boolean equals(Object that) {
