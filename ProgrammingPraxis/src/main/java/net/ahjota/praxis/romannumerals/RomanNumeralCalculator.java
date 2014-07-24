67ae7cb96a18b4031a9bb9672099dbf93ffaf339
AJ Alon <alexander.joel.alon@gmail.com>
2014-07-24 16:21:29 -0700

    Roman#: stub class for the Roman Numeral Calculator

---
+++ ProgrammingPraxis/src/main/java/net/ahjota/praxis/romannumerals/RomanNumeralCalculator.java
@@ -0,0 +1,15 @@
+package net.ahjota.praxis.romannumerals;
+
+/**
+ * @author ajalon
+ */
+public class RomanNumeralCalculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String add(String... addends) {
+
+    }
+}
