60c97b0e1a959b465e72253de9e4df3b08720ca0
AJ Alon <alexander.joel.alon@gmail.com>
2014-07-27 16:55:27 -0700

    WordFreqs: stub

---
+++ ProgrammingPraxis/src/test/java/net/ahjota/praxis/wordfreqs/WordCounter.java
@@ -0,0 +1,14 @@
+package net.ahjota.praxis.wordfreqs;
+
+/**
+ * Write a program that takes a filename and a parameter n and prints the n
+ * most common words in the file, and the count of their occurrences, in
+ * descending order.
+ *
+ * @see <http://programmingpraxis.com/2009/03/10/word-frequencies/>
+ * http://programmingpraxis.com/2009/03/10/word-frequencies/</a>
+ *
+ * @author ajalon
+ */
+public class WordCounter {
+}
